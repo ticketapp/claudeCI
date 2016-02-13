80983006ea389b287de7e71bfc36a4a9c0531ff1
Simon Garnier <simongarnier07@hotmail.fr>
2016-02-13 18:41:19 +0100

    Hipchat notifications

--- runBuild.sh
+++ runBuild.sh
@@ -14,13 +14,23 @@ sbt_output=$(sbt "test")
 echo $sbt_output
 
 if [[ $(echo $sbt_output | tail -c 80 | grep success | wc -l) > 0 ]]; then
+	echo -e "\e[32mTests pass\e[0m";
+
         sbt dist;
 
         sshpass -p 9g2Myjdt scp -o StrictHostKeyChecking=no /master/server/target/universal/server-0.1-SNAPSHOT.zip root@51.254.122.113:/root/claude.zip;
 
         sshpass -p "9g2Myjdt" ssh -o StrictHostKeyChecking=no -t root@51.254.122.113 'unzip -o claude.zip;';
 
-        sshpass -p "9g2Myjdt" ssh -o StrictHostKeyChecking=no -t root@51.254.122.113 'service supervisor restart; sleep 2';
+        sshpass -p "9g2Myjdt" ssh -o StrictHostKeyChecking=no -t root@51.254.122.113 'service supervisor restart; sleep 5';
+
+	echo -e "\e[32mEverything worked well\e[0m";
+	
+	curl -d '{"color":"green","message":"Build success (yey)","notify":true,"message_format":"text"}' -H 'Content-Type: application/json' https://siloprod.hipchat.com/v2/room/1512094/notification?auth_token=LB0K8CkgPFy3Zq3nugKVwAnhTzr8dEYEhgullZs9
+
 else
-        echo "Tests did not pass";
+	echo -e "\e[31mTests did not pass\e[0m";
+
+	curl -d '{"color":"red","message":"Build failed (areyoukiddingme)","notify":true,"message_format":"text"}' -H 'Content-Type: application/json' https://siloprod.hipchat.com/v2/room/1512094/notification?auth_token=LB0K8CkgPFy3Zq3nugKVwAnhTzr8dEYEhgullZs9	
 fi
+
